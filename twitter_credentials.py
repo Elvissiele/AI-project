c41872dde8e85fa991f33df3bfcf92aa0b5aae95
Elvissiele <elvissiele@gmail.com>
2022-03-27 12:05:09 +0300

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = "1335636429939109893-tsswwC2uiAbti9v17XUgFAJFCr5sYJ"
+ACCESS_TOKEN_SECRET = "bamujYvkuBCkBW4RjYzfHMeNA5BT5QEt1s7j2xMYXejCM"
+CONSUMER_KEY = "6Qj3nmtZkDHNHVXMorzcHdW4w"
+CONSUMER_SECRET = "Yhn9qghgMAFLeclQmOl8vYwkMXrfWjRhdj8MJNCCPu9wdJnIEh"
